1733bae31c36f34dd46b2d5a967278c4e912aecd
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-08-10 22:33:44 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import { Container } from './App.styled';
 import FeedbackSection from './Section/Section';
 import Feedback from '../components/FeedbackOptions/FeedbackOptions';
 import Statistic from '../components/Statistics/Statistics';
 import Notification from '../components/Statistics/Notification';
 
-class App extends Component {
+class App1 extends Component {
   state = {
     good: 0,
     neutral: 0,
@@ -52,4 +52,36 @@ class App extends Component {
     );
   }
 }
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  handleBtn = option => {
+    useState(prevState => ({ [option]: prevState[option] + 1 }));
+    // console.log(option);
+  };
+  return (
+    <Container>
+      <FeedbackSection title="Please leave feedback">
+        <Feedback options={Object.keys(this.state)} onFeedbackBtn={handleBtn} />
+      </FeedbackSection>
+
+      <FeedbackSection title="Statistic">
+        {!totalFeedback ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={feedBackPercentage ? feedBackPercentage : 0}
+          />
+        )}
+      </FeedbackSection>
+    </Container>
+  );
+};
+
 export default App;
