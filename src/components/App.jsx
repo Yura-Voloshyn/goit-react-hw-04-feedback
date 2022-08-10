828f4ea0d1b7cb9854d4046a3acc44c5c4715155
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-08-10 19:22:17 +0300

    test-task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -53,3 +53,71 @@ class App extends Component {
   }
 }
 export default App;
+
+// const salaries1 = {
+//   Manager: { salary: 1000, tax: '10%' },
+//   Designer: { salary: 600, tax: '30%' },
+//   Artist: { salary: 1500, tax: '15%' },
+// };
+// const team1 = [
+//   { name: 'Misha', specialization: 'Manager' },
+//   { name: 'Max', specialization: 'Designer' },
+//   { name: 'Vova', specialization: 'Designer' },
+//   { name: 'Leo', specialization: 'Artist' },
+// ];
+// const salaries2 = {
+//   TeamLead: { salary: 1000, tax: '99%' },
+//   Architect: { salary: 9000, tax: '34%' },
+// };
+// const team2 = [
+//   { name: 'Alexander', specialization: 'TeamLead' },
+//   { name: 'Gaudi', specialization: 'Architect' },
+//   { name: 'Koolhas', specialization: 'Architect' },
+//   { name: 'Foster', specialization: 'Architect' },
+//   { name: 'Napoleon', specialization: 'General' },
+// ];
+
+// function calculateTeamFinanceReport(salaries, team) {
+//   let totalSalary = 0;
+//   const totalBudget = { totalBudgetTeam: 0 };
+//   const teamPositions = Object.keys(salaries);
+
+//   if (teamPositions.length < 1 || teamPositions.length > 10) {
+//     alert('minimum specializations amount is 1, maximum up to 10');
+//     return;
+//   }
+//   if (team.length < 1 || team.length > 100) {
+//     alert('minimal team size is 1, maximum up to 100');
+//     return;
+//   }
+
+//   for (const position of teamPositions) {
+//     const salary = salaries[position].salary;
+//     const tax = Number(salaries[position].tax.replace(/[^0-9]/g, ''));
+//     if (!Number.isInteger(salary) || salary < 100 || salary > 100000) {
+//       alert(' salary after tax should be integer; min: 100, max: 100000');
+//       return;
+//     }
+//     if (!Number.isInteger(tax) || tax < 0 || tax > 99) {
+//       alert('tax percent should be integer; min: 0%, max: 99%');
+//       return;
+//     }
+//     const positionCounter = team.filter(
+//       member => member.specialization === position
+//     ).length;
+//     const salaryWithTaxCalculate = Math.abs((salary / (100 - tax)) * 100);
+
+//     totalBudget[`totalBudget${position}`] = Math.floor(
+//       positionCounter * salaryWithTaxCalculate
+//     );
+//     totalSalary += positionCounter * salaryWithTaxCalculate;
+//   }
+//   totalBudget.totalBudgetTeam = Math.floor(totalSalary);
+//   return totalBudget;
+// }
+
+// const financeReport1 = calculateTeamFinanceReport(salaries1, team1);
+// console.log(JSON.stringify(financeReport1));
+
+// const financeReport2 = calculateTeamFinanceReport(salaries2, team2);
+// console.log(JSON.stringify(financeReport2));
